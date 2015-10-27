16abccdf499f520e33446bc0db96ea6cf2d7a1db
Igor Lobanov <bniwredyc@gmail.com>
2015-10-27 16:58:37 +0300

    Fixed replacement of multiple U+2028 and U+2029

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // slashes escaped to prevent '</script>' tag output in JSON within
 // script tags. See http://stackoverflow.com/questions/66837/when-is-a-cdata-section-necessary-within-a-script-tag/1450633#1450633
 module.exports = function(obj, replacer, space) {
-    return JSON.stringify(obj, replacer, space).replace(new RegExp('/', 'g'), '\\/')
+    return JSON.stringify(obj, replacer, space).replace(/\//g, '\\/')
     // Escape u2028 and u2029
     // http://timelessrepo.com/json-isnt-a-javascript-subset
     // https://github.com/mapbox/tilestream-pro/issues/1638
-    .replace("\u2028", "\\u2028").replace("\u2029", "\\u2029");
+    .replace(/\u2028/g, "\\u2028").replace(/\u2029/g, "\\u2029");
 };
